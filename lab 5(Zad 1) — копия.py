ab24463f731b0c06aa120cc9ae61e0915d903ec7
Ruslan <113594889+Leon13Mozart@users.noreply.github.com>
2022-09-21 22:06:40 +0300

    Add files via upload

---
+++ lab 5(Zad 1) — копия.py
@@ -0,0 +1,12 @@
+import math
+x = float(input("Задайте x:   "))
+y = 0.0
+if x>=3:
+    y=math.exp(x>=3)
+elif 0<=x and x<3:
+    y = math.pow(0<=x<3)
+elif x<3:
+    y = math.exp(x<3)
+else:
+    print("Ошибка")
+print(y)
