2409c720f00ef3f52c609dda31bd6809c3e304ad
Ruslan <113594889+Leon13Mozart@users.noreply.github.com>
2022-09-26 08:32:36 +0300

    Update lab5(zad 2) — копия.py

--- lab5(zad 2) — копия.py
+++ lab5(zad 2) — копия.py
@@ -1,18 +1,19 @@
-n = float(input("Введите число 1 -4: "))
+n = int(input("Введите число 1 -4: "))
 a = 1#Лето
 b = 2#Осень
 c = 3#Зима
 d = 4#Весна
 x = 4.1#Левое число
-if 1<n<3<4:
+if (1<n<3<4):
     print("Осень")
-elif n<b and b<c and c<d:
+elif (n<b and b<c and c<d):
     print("Лето")
-elif n<d and n>b and b>a:
+elif (n<d and n>b and b>a):
     print("Зима")
 elif n>c and c>b and b>a and x>n:
     print("Весна")
-elif n>x and x>d and x>c and x>b and x>a:
+elif (n>x and x>d and x>c and x>b and x>a):
     print("Число не верное, введите от 1 до 4")
 else:
-    print("Введите заново")
+    print("")
+
