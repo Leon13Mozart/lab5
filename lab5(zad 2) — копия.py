fd1245d8bfe05044d42c6aed58976b7fefc7987b
Ruslan <113594889+Leon13Mozart@users.noreply.github.com>
2022-09-26 10:46:55 +0300

    Update lab5(zad 2) — копия.py

--- lab5(zad 2) — копия.py
+++ lab5(zad 2) — копия.py
@@ -4,15 +4,15 @@ b = 2#Осень
 c = 3#Зима
 d = 4#Весна
 x = 4.1#Левое число
-if (1<n<3<4):
+if 1<n and n<3 and 3<4:
     print("Осень")
-elif (n<b and b<c and c<d):
+elif n<b and b<c and c<d:
     print("Лето")
-elif (n<d and n>b and b>a):
+elif n<d and n>b and b>a:
     print("Зима")
 elif n>c and c>b and b>a and x>n:
     print("Весна")
-elif (n>x and x>d and x>c and x>b and x>a):
+elif n>x and x>d and x>c and x>b and x>a:
     print("Число не верное, введите от 1 до 4")
 else:
     print("")
